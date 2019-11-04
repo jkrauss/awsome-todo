4d9d23f981e9d4e5ebbc6e6b666743493ab5d3d1
Jonas Krauss <jonas.krauss@dating-gmbh.de>
2019-11-04 16:31:22 +0100

    start building with quasar

--- quasar.conf.js
+++ quasar.conf.js
@@ -41,7 +41,13 @@ module.exports = function (ctx) {
       //            (not treeshaking Quasar; biggest bundle size; convenient)
       all: 'auto',
 
-      components: [],
+      components: [
+        'QHeader',
+        'QFooter',        
+        'QTabs',
+        'QTab',
+        'QRouteTab'
+      ],
       directives: [],
 
       // Quasar plugins
